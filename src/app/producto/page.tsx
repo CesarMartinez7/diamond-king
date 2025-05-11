17ef6223c5b77ca0e83e797c0e9919c4ab9d650a
CesarMartinez <cesarwamartinez@gmail.com>
2025-05-10 19:24:27 -0500

    Añadiendo componentes

---
+++ src/app/producto/page.tsx
@@ -0,0 +1,82 @@
+"use client";
+import ShinyText from "@/components/title";
+import { ReviewComponents } from "@/components/review";
+import PixelTransition from "@/components/PixelCard";
+
+export default function ProductosPages() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <div>
+        <PixelTransition
+          firstContent={
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg"
+              alt="default pixel transition content, a cat!"
+              style={{ width: "100%", height: "100%", objectFit: "cover" }}
+            />
+          }
+          secondContent={
+            <div
+              style={{
+                width: "100%",
+                height: "100%",
+                display: "grid",
+                placeItems: "center",
+                backgroundColor: "#111",
+              }}
+            >
+              <p
+                style={{ fontWeight: 900, fontSize: "3rem", color: "#ffffff" }}
+              >
+                Meow!1
+              </p>
+            </div>
+          }
+          gridSize={12}
+          pixelColor="#ffffff"
+          animationStepDuration={0.4}
+          className="custom-pixel-card"
+        />
+        <ReviewComponents
+          items={testimonials}/>
+        <ShinyText text="Productos" speed={5} className="text-6xl font-bold" />
+      </div>
+      <div></div>
+    </div>
+  );
+}
+
+
+
+const testimonials = [
+  {
+    quote:
+      "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
+    name: "Charles Dickens",
+    title: "A Tale of Two Cities",
+  },
+  {
+    quote:
+      "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
+    name: "William Shakespeare",
+    title: "Hamlet",
+  },
+  {
+    quote: "All that we see or seem is but a dream within a dream.",
+    name: "Edgar Allan Poe",
+    title: "A Dream Within a Dream",
+  },
+  {
+    quote:
+      "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
+    name: "Jane Austen",
+    title: "Pride and Prejudice",
+  },
+  {
+    quote:
+      "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
+    name: "Herman Melville",
+    title: "Moby-Dick",
+  },
+];
+
